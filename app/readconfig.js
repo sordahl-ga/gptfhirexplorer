c75d70cd56b28cffab0b8ce43d23bef28ca6146f
Elena Neroslavskaya <lenisha@gmail.com>
2023-06-12 17:40:03 -0400

    add cookie encoding

--- app/readconfig.js
+++ app/readconfig.js
@@ -2,4 +2,8 @@ function getCookie(key) {
     var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
     return keyValue ? keyValue[2] : null;
 }
-const config = JSON.parse(getCookie("config"));
+
+const encodedValue = getCookie("config");
+const decodedValue = atob(encodedValue);
+
+const config = JSON.parse(decodedValue);
